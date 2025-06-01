faee8c094b2ae8c5c673408108d20af31dbda594
kaushal328 <panchalkaushal636@gmail.com>
2025-06-01 22:18:01 +0530

    Changes

---
+++ spring/src/in/sp/beans/Address.java
@@ -0,0 +1,7 @@
+package in.sp.beans;
+
+public class Address {
+		private String homeName;
+		private String phoneNo;
+		private int pincode;
+}
---
+++ spring/src/in/sp/beans/Students.java
@@ -0,0 +1,7 @@
+package in.sp.beans;
+
+public class Students {
+			private String name;
+			private int rollno;
+			private String email;
+}
